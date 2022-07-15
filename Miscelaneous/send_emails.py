1176cf6a78e313a1049e61edadd374e6d3b42c48
gilnribeiro <73843475+gilnribeiro@users.noreply.github.com>
2022-07-15 14:37:29 +0100

    Update Send email

--- Miscelaneous/send_emails.py
+++ Miscelaneous/send_emails.py
@@ -5,12 +5,39 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-def email_new(df):
+def email_new(df, text):
+    message = MIMEMultipart()
+    message["Subject"] = "Flagged Data"
+    message["From"] = creds.sender
+    message["To"] = creds.recipient
+    message["Content"] = "This is a test email"
+    message.attach(MIMEText(text, "plain"))
+
+    html = MIMEText(df.to_html(index=False), "html")
+    message.attach(html)
+    with smtplib.SMTP("smtp.outlook.com", 587) as server:
+        server.starttls()
+        server.login(creds.sender, creds.password)
+        server.sendmail(creds.sender, creds.recipient, message.as_string())
+
+
+if __name__ == "__main__":
+    df = pd.DataFrame({"Test": [1, 2, 3], "Data": [123, "4124", "ADDas"]})
+    text = """
+import creds
+import smtplib
+import pandas as pd
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def email_new(df, text):
     message = MIMEMultipart()
     message['Subject'] = 'Flagged Data'
     message['From'] = creds.sender
     message['To'] = creds.recipient
-    message.preamble = 'This is a test email'
+    message['Content'] = 'This is a test email'
+    message.attach(MIMEText())
     
     html = MIMEText(df.to_html(index=False), "html")
     message.attach(html)
@@ -23,4 +50,7 @@ def email_new(df):
         
 if __name__ == '__main__':
     df = pd.DataFrame({'Test': [1,2,3], 'Data':[123, '4124','ADDas']})
-    email_new(df)
+    text = 'Hello there!'
+    email_new(df, text)
+    """
+    email_new(df, text)
